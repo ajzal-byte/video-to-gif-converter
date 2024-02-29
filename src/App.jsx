f32b25d34ff62e5d7ae5592414bdf169e21149fe
ajzal-byte <ajzalaju10@gmail.com>
2024-02-29 13:01:54 +0530

    Functionaities added

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import { saveAs } from 'file-saver';
 
 import { Button, Dbutton, Header, Inputfile, Inputvideo, Resultimg } from './components';
 
+// Create the FFmpeg instance and load it. log set as 'true' to  enables logging for the FFmpeg library
 const ffmpeg = createFFmpeg({log: true});
 
 function App() {
   
   const [ready, setReady] = useState(false);
   const [video, setVideo] = useState();
+  const [gif, setGif] = useState();
 
   const load = async () =>{
     await ffmpeg.load();
@@ -20,6 +22,42 @@ function App() {
   useEffect(() => {
     load();
   }, []);
+
+  const convertToGif = async () =>{
+    // Write the .mp4 to the FFmpeg file system
+    ffmpeg.FS("writeFile", "video.mp4", await fetchFile(video));
+
+    // Run the FFmpeg command-line tool, converting the .mp4 to .gif file
+    await ffmpeg.run(
+      "-i",
+      "video.mp4",
+      "-t",
+      "2.5s",
+      "-ss",
+      "2.0",
+      "-f",
+      "gif",
+      "output.gif"
+    );
+
+    // Read the .gif file back from FFmpeg file system
+    const data = ffmpeg.FS("readFile", "output.gif");
+    const url = URL.createObjectURL(
+      new Blob([data.buffer], {type: "image/gif"})
+    );
+    setGif(url);
+  }
+
+  const download = (e) =>{
+    try {
+      console.log(e.target.href);
+      // Remove the file extension from the video name
+      const videoNameWithoutExtension = video.name.split('.').slice(0, -1).join('.');
+      saveAs(e.target.href, videoNameWithoutExtension + ".gif");
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
 
   return {ready} ? (
